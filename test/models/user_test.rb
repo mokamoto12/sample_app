b7f37a42dc21bdbe8164f238d8527511f805b0a0
mokamoto12 <mokamoto1208@gmail.com>
2017-07-17 20:00:50 +0900

    Add user model validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,4 +8,14 @@ class UserTest < ActiveSupport::TestCase
   test "shoule be valid" do
     assert @user.valid?
   end
+
+  test "name shoule be present" do
+    @user.name = "  "
+    assert_not @user.valid?
+  end
+
+  test "email shoule be present" do
+    @user.email = "  "
+    assert_not @user.valid?
+  end
 end
