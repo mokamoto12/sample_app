1c51f5b054776fec2ea8a4723a570779e63d0604
mokamoto12 <okamoto_masahiro@aratana.jp>
2017-07-19 18:30:12 +0900

    Update flash tag render method

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -14,4 +14,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select '#error_explanation > ul > li', 6
     assert_select '.field_with_errors', 6
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post signup_path, params: { user: { name: "test",
+                                          email: "test@example.com",
+                                          password: "test12",
+                                          password_confirmation: "test12" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
 end
